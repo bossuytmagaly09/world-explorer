9381b3191e7f58d35b0a4953977a8c8f25cbb916
Magaly Bossuyt <bossuytmagaly2@gmail.com>
2025-11-23 15:51:42 +0100

    storage

--- src/js/services/storageService.js
+++ src/js/services/storageService.js
@@ -8,7 +8,18 @@ export function loadFavorites() {
 // - lees uit localStorage met STORAGE_KEY
 // - parse JSON
 // - ga veilig om met null / parse errors
-    return [];
+    try{
+        const json = localStorage.getItem(STORAGE_KEY);
+        if(!json) return [];
+
+        const data = json.parse(json);
+
+        return array.isArray(data) ? data : [];
+    }catch (err){
+        console.error("Fout bij het lezen van favorieten.", err);
+        return [];
+    }
+
 }
 /**
  * Schrijf favorieten naar localStorage.
@@ -18,4 +29,12 @@ export function saveFavorites(favorites) {
 // TODO:
 // - stringify favorites
 // - schrijf naar localStorage
+    export function saveFavorites(favorites) {
+        try {
+            const json = JSON.stringify(favorites);
+            localStorage.setItem(STORAGE_KEY, json);
+        } catch (err) {
+            console.error("Fout bij opslaan van favorieten:", err);
+        }
+    }
 }
